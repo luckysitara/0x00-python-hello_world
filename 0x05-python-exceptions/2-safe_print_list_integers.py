1d95a18705e4e802430c30aa8f36b6992099de4b
luckysitara <udoessienprecious@gmail.com>
2023-08-28 06:01:57 -0700

    correcting the errors in 2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     index = 0
-    for item in range (x):
+    for item in range(x):
         try:
             print("{}".format(my_list=[item], end=''))
             index++
