3b79947345ca546f762e44d611b12aa6251082b0
luckysitara <udoessienprecious@gmail.com>
2023-08-28 05:55:11 -0700

    found errors in 2-safe_print_list_integer.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,5 +1,5 @@
- #!/usr/bin/python3
- def safe_print_list_integers(my_list=[], x=0):
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
     index = 0
     for item in range (x):
         try:
