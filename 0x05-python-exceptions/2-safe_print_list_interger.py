aa914b93bdf74844876adf9f5680a3b03baa183d
luckysitara <udoessienprecious@gmail.com>
2023-08-28 05:49:57 -0700

    finishing 2-safe_print_list_interger.py

---
+++ 0x05-python-exceptions/2-safe_print_list_interger.py
@@ -0,0 +1,11 @@
+ #!/usr/bin/python3
+ def safe_print_list_integers(my_list=[], x=0):
+    index = 0
+    for item in range (x):
+        try:
+            print("{}".format(my_list=[item], end=''))
+            index++
+        except (ValueError, TypeError)
+        pass
+    print()
+    return index
