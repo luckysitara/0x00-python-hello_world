6988386ac1cbc9a2f3ed1820a1c276bfeabaf7a0
luckysitara <udoessienprecious@gmail.com>
2023-08-29 09:13:22 -0700

    103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-import math
-
-
 class MagicClass:
     def __init__(self, radius):
         self.__radius = 0
@@ -10,7 +7,7 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
-        return self.__radius ** 2 * math.pi
+        return self.__radius * self.__radius * 3.14
 
     def circumference(self):
-        return 2 * math.pi * self.__radius
+        return 2 * 3.14 * self.__radius
