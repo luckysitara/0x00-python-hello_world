548525c81b27f985481c8615966360a4a5c1a694
luckysitara <udoessienprecious@gmail.com>
2023-08-29 08:50:14 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""File that defines a square"""
+
+
+class Square:
+    """a class named Square that defines a square"""
+    def __init__(self, size+None):
+        """Initializing private methods or attribute size"""
+        self.__size = size
