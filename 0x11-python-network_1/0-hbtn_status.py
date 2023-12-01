42850be09cd2ba7a38289791c62ff709aafc220a
luckysitara <udoessienprecious@gmail.com>
2023-12-01 15:09:31 -0800

    still getting the same error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,9 +6,9 @@ import urllib
 #from urllib import request
 
 url = 'https://intranet.hbtn.io/status'
-#with request.urlopen(url) as response:
-#    the_page = response.read()
-#    print('Body response:')
-#    print('\t- type: {}'.format(type(the_page)))
-#    print('\t- content: {}'.format(the_page))
-#    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+with urllib.request.urlopen(url) as response:
+    the_page = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(the_page)))
+    print('\t- content: {}'.format(the_page))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
