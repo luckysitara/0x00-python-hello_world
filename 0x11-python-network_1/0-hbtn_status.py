545b798383dfca6093d1b90988f46a269c54e46f
luckysitara <udoessienprecious@gmail.com>
2023-12-01 15:47:05 -0800

    again it comes

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ url = 'https://alx-intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     status_page = response.read()
     print('Body response:')
-#    print('\t- type: {}'.format(type(status_page)))
+    print('\t- type: {}'.format(type(status_page)))
     print('\t- content: {}'.format(status_page))
     print('\t- utf8 content: {}'.format(status_page.decode('utf-8')))
 
