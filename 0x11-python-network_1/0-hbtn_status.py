3adc4ad059688f31bece6a51bc2af965f3c9f0bd
luckysitara <udoessienprecious@gmail.com>
2023-12-01 15:29:51 -0800

    again

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ url = 'https://intranet.hbtn.io/status'
 #with re.urlopen(url) as response:
 with request.urlopen(url) as response:
     the_page = response.read()
-'''    print('Body response:')
-    print('\t- type: {}'.format(type(the_page)))
-    print('\t- content: {}'.format(the_page))
-    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))'''
+#    print('Body response:')
+#    print('\t- type: {}'.format(type(the_page)))
+#    print('\t- content: {}'.format(the_page))
+#    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
