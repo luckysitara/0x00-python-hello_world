fcc332c7dc42a8465fc5b80efeb86fabdb464c4e
luckysitara <udoessienprecious@gmail.com>
2023-12-01 15:28:19 -0800

    new test

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,14 @@
 '''
 Script that fetches https://intranet.hbtn.io/status
 '''
-from urllib import request as re
-#from urllib import request
+#from urllib import request as re
+import urllib
 
 url = 'https://intranet.hbtn.io/status'
-with re.urlopen(url) as response:
-#with request.urlopen(url) as response:
+#with re.urlopen(url) as response:
+with request.urlopen(url) as response:
     the_page = response.read()
-    print('Body response:')
+'''    print('Body response:')
     print('\t- type: {}'.format(type(the_page)))
     print('\t- content: {}'.format(the_page))
-    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))'''
