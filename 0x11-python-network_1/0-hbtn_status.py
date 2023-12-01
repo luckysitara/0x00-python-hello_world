fe17c2d6d902db0ecefa9abb81de58a49a06b72a
luckysitara <udoessienprecious@gmail.com>
2023-12-01 15:45:15 -0800

    error all fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-Script that fetches https://alx-intranet.hbtn.io/status
+This is the Script that fetches https://alx-intranet.hbtn.io/status written by bughacker
 """
 import urllib.request
 
 url = 'https://alx-intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
-    the_page = response.read()
+    status_page = response.read()
     print('Body response:')
-    print('\t- type: {}'.format(type(the_page)))
-    print('\t- content: {}'.format(the_page))
-    print('\t- utf8 content: {}'.format(the_page.decode('utf-8')))
+#    print('\t- type: {}'.format(type(status_page)))
+    print('\t- content: {}'.format(status_page))
+    print('\t- utf8 content: {}'.format(status_page.decode('utf-8')))
 
