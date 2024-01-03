1398ab9697b5daa887ca86a57be3a5d458c84c99
luckysitara <udoessienprecious@gmail.com>
2024-01-03 05:43:45 -0800

    standard --fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 
-const filePath = process.argv[2]
+const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
-    console.log(data)
+    console.log(data);
   }
-})
+});
